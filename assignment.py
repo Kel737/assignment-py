d7c214cee5d0d0c8abe21393a6879a09b0453357
Kelvin Njoroge <kinyanjuikkelvin@gmail.com>
2021-06-10 19:11:59 +0300

    py assignment

---
+++ assignment.py
@@ -0,0 +1,19 @@
+# write a program to convert a users input from kgs to lbs and vice-versa
+
+kgs = int(input("Enter weight in kgs: "))
+pounds = kgs/0.90
+
+print("The weight in pounds is", round(kgs, 1))
+
+
+pounds = int(input("Enter weight in Pounds: "))
+kgs = pounds/0.90
+
+print("The weight in kgs is", round(kgs, 1))
+
+
+# find the total price
+
+def price(numbers):
+    total = 0
+print(sum((8, 2, 3, 0, 7)))
